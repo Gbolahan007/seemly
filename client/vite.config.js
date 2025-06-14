2194b32de8a974548b27b6a3250fbaf5864a4b51
Gbolahan007 <lawalomogbolahan08@gmail.com>
2025-06-14 01:29:33 +0100

    Fix: added base path in vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./", 
   plugins: [react()],
   build: {
-    outDir: "dist", // 👈 Ensure Vercel can find this folder
+    outDir: "dist",
   },
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx"],
